7e0d8091c8ca7652addadb313cd1545b1e25c8ee
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-02-18 17:54:54 +0100

    end task 02

--- 02/app.js
+++ 02/app.js
@@ -1,52 +1,39 @@
+"use strict";
 class UUID {
-    static count: number = 0;
-
-    static generate(): string {
+    static generate() {
         const generateSegment = () => {
-            const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';  
+            const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
             let segment = '';
-   
             for (let i = 0; i < 4; i++) {
-                const randomIndex = Math.floor(Math.random() * characters.length);  
-                segment += characters[randomIndex]; 
+                const randomIndex = Math.floor(Math.random() * characters.length);
+                segment += characters[randomIndex];
             }
-            
             return segment;
         };
-
         const uuid = `${generateSegment()}-${generateSegment()}-${generateSegment()}-${generateSegment()}`;
-        UUID.count++; 
+        UUID.count++;
         return uuid;
     }
-
-    static validate(uuid: string): boolean {
+    static validate(uuid) {
         const regex = /^[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}$/;
         return regex.test(uuid);
     }
 }
-
-abstract class Product {
-    uuid: string;
-
+UUID.count = 0;
+class Product {
     constructor() {
         this.uuid = UUID.generate();
     }
 }
-
 class Book extends Product {
-    author: string;
-    title: string;
-
-    constructor(title: string, author: string) {
-        super(); 
+    constructor(title, author) {
+        super();
         this.title = title;
         this.author = author;
     }
 }
-
 const book1 = new Book('title2', 'author2');
 const book2 = new Book('title2', 'author2');
-
-console.log(book1.uuid); 
-console.log(book2.uuid); 
-console.log(UUID.count); 
+console.log(book1.uuid);
+console.log(book2.uuid);
+console.log(UUID.count);
