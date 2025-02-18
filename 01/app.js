ac6072185820cdd62c7628a5310522ae8ef190e3
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-02-18 16:53:42 +0100

    end task 01

--- 01/app.js
+++ 01/app.js
@@ -1,45 +1,35 @@
+"use strict";
 class User {
-    private _email: string;
-    private _password: string = '';
-
-    constructor(email: string, password: string) {
+    constructor(email, password) {
+        this._password = '';
         this._email = email;
         hashPassword(password).then((hashedPassword) => {
             this._password = hashedPassword;
         });
     }
-
-    set email(value: string) {
+    set email(value) {
         this._email = value;
     }
-
-    get email(): string {
+    get email() {
         return this._email;
     }
-
-    async check(email: string, password: string): Promise<boolean> {
+    async check(email, password) {
         const hashedPassword = await hashPassword(password);
         console.log("Hash of entered password:", hashedPassword);
         return this._email === email && this._password === hashedPassword;
     }
 }
-
-
-async function hashPassword(password: string): Promise<string> {
+async function hashPassword(password) {
     const encoder = new TextEncoder();
     const data = encoder.encode(password);
     const hashBuffer = await crypto.subtle.digest('SHA-256', data);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
 }
-
 const user = new User('agnieszka@example.com', 'goodpassword');
-
-
 user.check('agnieszka@example.com', 'goodpassword').then(result => {
-    console.log(result); 
+    console.log(result);
 });
-
 user.check('agnieszka@example.com', 'wrongpassword').then(result => {
-    console.log(result); 
+    console.log(result);
 });
