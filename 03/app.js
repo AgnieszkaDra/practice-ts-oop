c1a86ff4603ad28478545ee1cfee0b4c5035f657
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-02-18 19:10:24 +0100

    end task 03

--- 03/app.js
+++ 03/app.js
@@ -1,29 +1,22 @@
-abstract class CustomNotification {
-    abstract send(): void;
-
-    static notify(notifications: CustomNotification[]): void {
+"use strict";
+class CustomNotification {
+    static notify(notifications) {
         console.log(notifications);
         notifications.forEach(notification => notification.send());
     }
 }
-
 class EmailNotification extends CustomNotification {
-    send(): void {
+    send() {
         console.log("Sending email notification");
     }
 }
-
 class SMSNotification extends CustomNotification {
-    send(): void {
+    send() {
         console.log("Sending SMS notification");
     }
 }
-
-
 const email = new EmailNotification();
 email.send();
-
 const sms = new SMSNotification();
 sms.send();
-
 CustomNotification.notify([email, sms]);
